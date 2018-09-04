c2ece0dae8e57fbb3b1c65fd3fea7114749d87bb
yuqing <2711676102@qq.com>
2018-09-04 11:04:06 +0800

    将 svg 代码整理到 icon.vue

---
+++ src/icon.vue
@@ -0,0 +1,21 @@
+<template>
+    <svg class="g-icon">
+        <use :xlink:herf="`#i-${name}`"></use>
+    </svg>
+</template>
+
+<script>
+export default {
+    props: ['name'],
+    data() {
+        return {
+        }
+    }
+}
+</script>
+<style lang="scss">
+    .g-icon {
+        width: 1em;
+        height: 1em;
+    }
+</style>
