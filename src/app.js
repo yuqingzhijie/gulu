61126f341b705e54941c999b3ad663a64ec5c8dc
yuqing <2711676102@qq.com>
2018-09-04 18:14:05 +0800

    添加button-group,marginleft-1有bug，改为-2

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './buttonGroup'
 
 Vue.component('g-icon', Icon)
 Vue.component('g-button', Button)
+Vue.component('g-button-group', ButtonGroup)
 
 new Vue({
     el: '#app',
---
+++ src/buttonGroup.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="g-button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {},
+    props: {},
+    data() {
+        return {
+        }
+    },
+    methods: {},
+}
+</script>
+<style lang="scss">
+    .g-button-group {
+        display: inline-flex;
+        vertical-align: middle;
+
+        >.g-button {
+            border-radius: 0;
+            &:not(:first-child) {
+                margin-left: -2px;
+            }
+            // &:not(:first-child) {
+            //     border-left: none;
+            // }
+
+            &:first-child {
+                border-top-left-radius: var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+            &:last-child {
+                border-top-right-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+            &:hover {
+                position: relative;
+                z-index: 1;
+            }
+        }
+    }
+</style>
