e0f3e935d96bd8bef049fd7a5e0a12bea3c4e11e
yuqing <2711676102@qq.com>
2018-09-06 16:45:18 +0800

    puppeteer自动测试

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './buttonGroup'
+import chai from 'chai'
+import spies from 'chai-spies'
+
+chai.use(spies)
 
 Vue.component('g-icon', Icon)
 Vue.component('g-button', Button)
@@ -18,3 +22,99 @@ new Vue({
         test() {console.log('test');}
     }
 })
+
+
+const expect = chai.expect
+
+try {
+    //单元测试
+    {
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'settings'
+            }
+        })
+        vm.$mount()
+        let useElement = vm.$el.querySelector('use')
+        let href = useElement.getAttribute('xlink:href')
+
+        expect(href).to.eq('#i-settings')
+        vm.$el.remove()
+        vm.$destroy()
+    }
+
+    {
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'settings',
+                loading: true
+            }
+        })
+        vm.$mount()
+        let useElement = vm.$el.querySelector('use')
+        let href = useElement.getAttribute('xlink:href')
+
+        expect(href).to.eq('#i-loading')
+        vm.$el.remove()
+        vm.$destroy()
+    }
+
+    {
+        let div = document.createElement('div')
+        document.body.appendChild(div)
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'settings'
+            }
+        })
+        vm.$mount(div)
+        let svg = vm.$el.querySelector('svg')
+        let {order} = window.getComputedStyle(svg)
+        expect(order).to.eq('1')
+        vm.$el.remove()
+        vm.$destroy()
+    }
+
+    {
+        const div = document.createElement('div');
+        document.body.appendChild(div);
+        const Constructor = Vue.extend(Button);
+        const vm = new Constructor({
+            propsData: {
+                icon: 'settings',
+                iconPosition: 'right'
+            }
+        })
+
+        vm.$mount(div)
+        let svg = vm.$el.querySelector('svg')
+        let {order} = window.getComputedStyle(svg)
+        expect(order).to.eq('2')
+        vm.$el.remove()
+        vm.$destroy()
+    }
+    {
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'settings',
+            }
+        })
+
+        vm.$mount()
+        let spy = chai.spy(function(){})
+        vm.$on('click', spy)
+        let button = vm.$el;
+        button.click();
+        expect(spy).to.have.been.called();
+    }
+} catch(error) {
+    window.errors = [error]
+} finally {
+    window.errors && window.errors.forEach( err => {
+        console.log(error.message);
+    })
+}
--- src/buttonGroup.vue
+++ src/buttonGroup.vue
@@ -12,6 +12,14 @@ export default {
         return {
         }
     },
+    mounted() {
+        for (let node of this.$el.children) {
+            let name = node.nodeName.toLowerCase();
+            if (name !== 'button') {
+                console.warn(`g-button-group 的子元素应该全是g-button，但你写的是${name}`);
+            }
+        }
+    },
     methods: {},
 }
 </script>
